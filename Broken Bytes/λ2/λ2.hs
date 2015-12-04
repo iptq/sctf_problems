5c7b4aa01ed4ed5f25604ddf2e66abe81808ecb1
Aaron Weiss <aaronweiss74@gmail.com>
2015-12-03 23:36:12 -0500

    Actual solution for Lambda 2.

--- Broken Bytes/λ2/λ2.hs
+++ Broken Bytes/λ2/λ2.hs
@@ -5,11 +5,11 @@ import Data.Function (on)
 f :: Integer -> Integer -> Integer
 f _ 0 = 1
 f n k 
- | n == k = 1
- | otherwise = f (n - 1) (k - 1) + f (n - 1) k
+  | n == k = 1
+  | otherwise = f (n - 1) (k - 1) + f (n - 1) k
 
 -- Solution:
--- f _ _ = 1538856934150604268232070485560186266135556504
+-- f n k = foldl (\acc i -> (acc * (n - i + 1)) `div` i) 1 [1 .. k]
 
 g :: String -> Integer -> String
 g s n = zipWith (fmap chr . xor `on` ord) s $ show n
