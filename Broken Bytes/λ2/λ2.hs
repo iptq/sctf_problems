85232e7bd7b3211c0450b1b44a3866fdbd5c82b6
Aaron Weiss <aaronweiss74@gmail.com>
2015-12-03 22:43:23 -0500

    Updated solution (RIP, bad mistake), and fixed binary.

--- Broken Bytes/λ2/λ2.hs
+++ Broken Bytes/λ2/λ2.hs
@@ -9,14 +9,13 @@ f n k
  | otherwise = f (n - 1) (k - 1) + f (n - 1) k
 
 -- Solution:
--- f _ 0 = 1
--- f n k = ((n + 1 + k) `div` k) * f n (k - 1)
+-- f _ _ = 1538856934150604268232070485560186266135556504
 
 g :: String -> Integer -> String
 g s n = zipWith (fmap chr . xor `on` ord) s $ show n
 
 h :: String
-h = "BUGPC^WGWCM_B\\SXFL"
+h = "BVG^C\\YOVFE]EX[]FK"
 
 main :: IO ()
 main = putStrLn . g h $ f 3274 17
